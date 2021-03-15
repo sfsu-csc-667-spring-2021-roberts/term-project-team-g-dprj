c954d15f82bed84059ca4cb40331433402a8f90f
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2021-03-14 22:27:29 -0700

    add test routes and fixed migration error

--- term-project-teamG/config/config.js
+++ term-project-teamG/config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "use_env_variables": "DATABSE_URL",
+    "use_env_variable": "DATABASE_URL",
     "dialect": "postgres"
   },
   "test": {
-    "use_env_variables": "DATABSE_URL",
+    "use_env_variable": "DATABASE_URL",
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variables": "DATABSE_URL",
+    "use_env_variable": "DATABASE_URL",
     "dialect": "postgres"
   },
 };
