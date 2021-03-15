cb94dfd225ae147d6e07c6ee36e6f8144697f57f
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2021-03-14 19:36:25 -0700

    installing heroku

--- term-project-teamG/app.js
+++ term-project-teamG/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+if(process.env.NODE_ENV === 'development') {
+  require("dotenv").config();
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
