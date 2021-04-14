0bafc011555101f31c1d790d032076112886717a
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2021-04-13 21:02:32 -0700

    css styling and logo

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,6 +39,63 @@ app.use('/unauthenticated/register', registerRouter);
 app.use('/authenticated/games', gameRouter);
 app.use('/authenticated/game-lobby', gameLobbyRouter);
 app.use('/authenticated/lobby', lobbyRouter);
+
+/* POST register listing. */
+app.post('/login', (req,res,next) => {
+  res.render('unauthenticated/login');
+});
+
+/* POST register listing. */
+app.post("/register", async (req,res) => {
+  let { name, email, password, password2 } = req.body;
+
+  console.log({
+    name, 
+    email, 
+    password, 
+    password2
+  });
+
+  let errors = []
+
+  if(!name || !email || !password || !password2) {
+    errors.push({ message: 'Please enter all fields'});
+  }
+
+  if(password.length < 8) {
+    errors.push({ message:'Password must be at least 8 characters'});
+  }
+
+  // if(password != password2) {
+  //   errors.push({ message:'Passwords do not match'});
+  // }
+
+  // if(errors.length > 0) {
+  //   res.render("unauthenticated/register", { errors }); // render to views folder 
+  // } else {
+
+  //   // validate password
+  //   let hashedPassword = await bcrypt.hash(password,10);
+  //   console.log(hashedPassword);
+
+  //   pool.query(
+  //     `SELECT * FROM users
+  //     WHERE email $1`, 
+  //     [email],
+  //     (err, results) => {
+  //       if(err){
+  //         throw err;
+  //       }
+  //       console.log(results.rows);
+
+  //       if(results.rows.length > 0) {
+  //         errors.push({ message: "Email already registered!"});
+  //         res.render("/register", { errors });
+  //       }
+  //     }
+  //   )
+  // }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/unauthenticated/register.js
+++ routes/unauthenticated/register.js
@@ -8,55 +8,55 @@ router.get("/", (req, res) => {
     res.render("unauthenticated/register");
 });
 
-router.post("/unauthenticated/register", async (req,res) => {
-    let { name, email, password, password2 } = req.body;
+// router.post("/unauthenticated/register", async (req,res) => {
+//     let { name, email, password, password2 } = req.body;
 
-    console.log({
-        name, 
-        email, 
-        password, 
-        password2
-    });
+//     console.log({
+//         name, 
+//         email, 
+//         password, 
+//         password2
+//     });
 
-    let errors = []
+//     let errors = []
 
-    if(!name || !email || !password || !password2) {
-        errors.push({ message: 'Please enter all fields'});
-    }
+//     if(!name || !email || !password || !password2) {
+//         errors.push({ message: 'Please enter all fields'});
+//     }
 
-    if(password.length < 8) {
-        errors.push({ message:'Password must be at least 8 characters'});
-    }
+//     if(password.length < 8) {
+//         errors.push({ message:'Password must be at least 8 characters'});
+//     }
 
-    if(password != password2) {
-        errors.push({ message:'Passwords do not match'});
-    }
+//     if(password != password2) {
+//         errors.push({ message:'Passwords do not match'});
+//     }
 
-    if(errors.length > 0) {
-        res.render("unauthenticated/register", { errors });
-    } else {
+//     if(errors.length > 0) {
+//         res.render("/unauthenticated/register", { errors });
+//     } else {
 
-        // validate password
-        let hashedPassword = await bcrypt.hash(password,10);
-        console.log(hashedPassword);
+//         // validate password
+//         let hashedPassword = await bcrypt.hash(password,10);
+//         console.log(hashedPassword);
 
-        pool.query(
-            `SELECT * FROM users
-            WHERE email $1`, 
-            [email],
-            (err, results) => {
-                if(err){
-                    throw err;
-                }
-                console.log(results.rows);
+//         pool.query(
+//             `SELECT * FROM users
+//             WHERE email $1`, 
+//             [email],
+//             (err, results) => {
+//                 if(err){
+//                     throw err;
+//                 }
+//                 console.log(results.rows);
 
-                if(results.rows.length > 0) {
-                    errors.push({ message: "Email already registered!"});
-                    res.render("unauthenticated/register", { errors });
-                }
-            }
-        )
-    }
-})
+//                 if(results.rows.length > 0) {
+//                     errors.push({ message: "Email already registered!"});
+//                     res.render("/unauthenticated/register", { errors });
+//                 }
+//             }
+//         )
+//     }
+// })
 
 module.exports = router;
