4693d0793ecf80014e855206da5fa63476f87fff
John Roberts <roberts.john@gmail.com>
2021-04-14 17:49:55 -0700

    [Fixes #12] Moving files around (#13)
    * Aggregated similar routes into a single module

--- routes/authenticated/games.js
+++ routes/authenticated/games.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../../db/connection');
+const db = require("../../db/connection");
 
-router.get("/:id", (req, res) => {
-    res.render("authenticated/game-lobby", { gameId: req.params.id });
+router.get("/:id", (request, response) => {
+  response.render("authenticated/game", { gameId: request.params.id });
+});
+
+router.get("/:id/lobby", (request, response) => {
+  response.render("authenticated/game-lobby", { gameId: request.params.id });
 });
 
 module.exports = router;
--- routes/unauthenticated/home.js
+++ routes/unauthenticated/home.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('unauthenticated/home', { username: req.params.username });
+router.get("/", (request, response) => {
+  response.render("unauthenticated/home", {
+    username: request.params.username,
+  });
 });
 
 module.exports = router;
