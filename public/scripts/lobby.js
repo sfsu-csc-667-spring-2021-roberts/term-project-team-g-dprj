d4b5e83db9f347084a1d9337291c2f5a92f0dec8
John Roberts <roberts.john@gmail.com>
2021-05-13 20:00:39 -0700

    * Lobby styling updated (#26)
    * Join game logic implemented to dynamically update game listing in lobby
    * Game logic update to prevent same user from joining multiple times

--- public/scripts/lobby.js
+++ public/scripts/lobby.js
@@ -1,12 +1,35 @@
 const channel = window.dprj.pusher.subscribe('chat-lobby');
+const listingChannel = window.dprj.pusher.subscribe('game-listing');
 
 const messageBox = document.querySelector('#message-box');
 const chatArea = document.querySelector('#messages');
+const gameList = document.querySelector('#game-list');
 
-channel.bind('message', ({ message, timestamp }) => {
-  console.log(message);
+let userId = undefined;
+// It's possible that a message could be received before this happened but :shrug:
+fetch('/users/whoami', {
+  headers: { 'Content-Type': 'application/json' },
+  credentials: 'include',
+})
+  .then((response) => response.json())
+  .then(({ id }) => (userId = id))
+  .catch((error) => {
+    console.error('Failed to retrive current user id:', error);
+  });
+
+channel.bind('message', ({ message, timestamp, user }) => {
   const div = document.createElement('div');
-  div.innerHTML = `<b>${moment(timestamp).fromNow()}</b> ${message}`;
+  console.log('userId', userId, 'user.id', user.id);
+  div.setAttribute('class', `message ${userId === user.id ? 'me' : 'not-me'}`);
+
+  const image = `<img class="user-icon" src="https://www.gravatar.com/avatar/${user.hash}?s=30" alt="${user.fullname}" />`;
+  const ts = `<b><span data-livestamp="${timestamp}"></span></b>`;
+
+  if (userId !== user.id) {
+    div.innerHTML = `${image}${ts} ${message}`;
+  } else {
+    div.innerHTML = `${message} ${ts}${image}`;
+  }
 
   chatArea.appendChild(div);
 });
@@ -30,6 +53,14 @@ messageBox.addEventListener('keyup', (event) => {
   }
 });
 
-document.querySelector('#message-submit').addEventListener('click', (event) => {
+document.querySelector('#message-submit').addEventListener('click', () => {
   sendMessage();
+});
+
+listingChannel.bind('added', ({ id, name }) => {
+  const li = document.createElement('li');
+  li.setAttribute('id', `join-game-${id}`);
+  li.innerHTML = `<a href="/games/${id}/join"> Join ${name}</a>`;
+
+  gameList.appendChild(li);
 });
